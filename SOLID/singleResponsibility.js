c7721a1119ee0b480a56064bffc8c32cca6c333b
surshreya <surshreya.0727@gmail.com>
2023-06-19 20:09:10 +0530

    SOLID principles

---
+++ SOLID/singleResponsibility.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+class Journal {
+  constructor() {
+    this.entries = {};
+    this.count = 0;
+  }
+
+  addEntry(text) {
+    let idx = ++this.count;
+    let entry = `${idx}: ${text}`;
+    this.entries[idx] = entry;
+    return idx;
+  }
+
+  removeEntry(idx) {
+    delete this.entries[idx];
+  }
+
+  viewEntries() {
+    return Object.values(this.entries).join("\n");
+  }
+}
+
+class PersistenceManager {
+  saveToFile(journal, filename) {
+    fs.writeFileSync(journal, filename);
+  }
+
+  loadFromFile() {}
+
+  loadFromURL() {}
+
+  saveToURL() {}
+}
+
+let journal1 = new Journal();
+journal1.addEntry("I woke at 7:00AM.");
+journal1.addEntry("It was raining cats and dogs in the morning");
+journal1.addEntry("I went to office");
+journal1.removeEntry(2);
+console.log(journal1.viewEntries());
